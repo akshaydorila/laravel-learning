e954f7d0a14e13eede69088f7a1b520c0fbe5820
akshaydorila <akshaydorila@gmail.com>
2026-03-11 16:20:13 +0530

    route concepts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// without param
+// Route::get('/greeting', function () {
+//     return "Hello World";
+// });
+
+// required param
+Route::get('/greeting/{name}', function ($name) {
+    return "Hello World $name";
+}); // ->where('name', '[A-Za-z]+')
+
+// optional param
+// Route::get('/greeting/{name?}', function ($name = null) {
+//     return "Hello World $name";
+// });
+
+// request data
+// Route::get('/request-data', function (Request $request) {
+//     // return $request->all();
+//     return $request->name; // get only name from request data
+// });
+
+// if route not exists, then fallback route will call
+Route::fallback(function () {
+    return 'Page Not Found';
 });

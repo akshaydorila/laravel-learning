a38375e3319852229e5b5927cd4671ea665227d7
akshaydorila <akshaydorila@gmail.com>
2026-03-13 16:34:09 +0530

    conditional style class, blade syntax

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,11 +16,36 @@ class TestController extends Controller
         // $input = $request->all();
         // $grade = $request->grade;
         $grade = $request->input('grade', 'A');
-        
-        // $data = ['name' => $name, 'subject' => $subject, 'grade' => $grade];
-        // return view('test', $data);
 
-        return view('test', compact('name', 'subject', 'grade'));
+        $data = [
+            'name' => $name,
+            'subject' => $subject,
+            'grade' => $grade,
+            'tag' => '<b>bold text</b>',
+            'count' => 0,
+            'users' => $this->usersData(),
+        ];
+        return view('test', $data);
+
+        // return view('test', compact('name', 'subject', 'grade'));
         // return view('test')->with(['name' => $name]);
+    }
+
+    private function usersData()
+    {
+        $users = [
+            ["type" => 2, "name" => "Aarav",  "number" => 2, "status" => 1],
+            ["type" => 3, "name" => "Vivaan", "number" => 3, "status" => 1],
+            ["type" => 1, "name" => "Aditya", "number" => 4, "status" => 0], // skipped because type = 1
+            ["type" => 2, "name" => "Krishna", "number" => 1, "status" => 1],
+            ["type" => 3, "name" => "Ishaan", "number" => 5, "status" => 1], // break happens here
+            ["type" => 2, "name" => "Aryan",  "number" => 6, "status" => 0],
+            ["type" => 3, "name" => "Rohan",  "number" => 7, "status" => 1],
+            ["type" => 1, "name" => "Kabir",  "number" => 8, "status" => 0],
+            ["type" => 2, "name" => "Arjun",  "number" => 9, "status" => 1],
+            ["type" => 3, "name" => "Dev",    "number" => 4, "status" => 0]
+        ];
+
+        return $users;
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,14 +5,71 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test View</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
 </head>
 
 <body>
-    <h1>Test View File</h1>
+    <!-- <h1>Test View File</h1>
     <h3>Name: {{ $name }}</h3>
-    <h3>Subject: {{ $subject }}</h3>
-    <div>Grade: {{ $grade }}</div>
+    <h3>Subject: {{ $subject }}</h3> -->
+    <div>Grade: {{ $grade > 15 ? 'Pass' : 'Fail' }}</div>
+    <!-- <div>{{ $tag }}</div> -->
+    <div>{!! $tag !!}</div> <!-- if you want to remove tags -->
 
+    <!-- IF Else statement -->
+    @if ($count === 1)
+    I have one record!
+    @elseif ($count > 1)
+    I have multiple records!
+    @else
+    I don't have any records!
+    @endif
+
+    <br>
+    <!-- Switch case -->
+    @switch($grade)
+    @case(1)
+    First case...
+    @break
+
+    @case(2)
+    Second case...
+    @break
+
+    @default
+    Default case...
+    @endswitch
+
+    <br> <br>
+    <!-- foreach method -->
+    @foreach ($users as $user)
+    <!-- @if ($user['type'] == 1)
+            @continue
+        @endif -->
+    @continue($user['type'] == 1)
+
+    <li>{{ $user['name'] }}</li>
+
+    @if($user['status'] == 1)
+    <li style="color: green;">Active</li>
+    @else
+    <li style="color: red;">Inactive</li>
+    @endif
+
+    <!-- @if ($user['number'] == 20)
+            @break
+        @endif -->
+    @break($user['number'] == 20)
+    @endforeach
+
+    <!-- CSS Class -->
+    <h1 class="red-color">Text 1</h1>
+    <h1 @class('cyan-color')>Text 2</h1>
+    <h1 @class([ 
+        'cyan-color'=> $grade > 10,
+        'red-color' => $grade <= 10, 
+        'green-color'=> $grade > 50
+    ])>Result</h1>
 </body>
 
 </html>
